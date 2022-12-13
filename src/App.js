e2c9702ac7e41f2ea08dff9ef7aabce21b97dc81
Ash Uppal <ashuppal@gmail.com>
2022-12-12 22:32:41 -0800

    lab 11

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import BestBooks from './BestBooks'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom'
+import Profile from './About'
+
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <>
         <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
           <Header />
           <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route path='/about' element={<Profile />}></Route>
+            <Route exact path='/' element={<BestBooks />}></Route> 
           </Routes>
           <Footer />
         </Router>
@@ -30,4 +35,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,28 +1,67 @@
 import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      search: ""
     }
+  }
+
+  componentDidMount = () => {
+    this.fetchBooks();
+  }
+  handleChange = (event) => {
+    this.setState({ search: event.target.value })
+  }
+  fetchBooks = async (bookTitle) => {
+    console.log('fetching book');
+    console.log(bookTitle);
+    let request = {
+      method: 'GET',
+      url: `${process.env.REACT_APP_SERVER_URL}/books`
+    }
+    if (bookTitle) {
+      request.url += `?title=${bookTitle}`
+    }
+    let response = await axios(request);
+    console.log(response);
+    this.setState({
+      books: response.data,
+    });
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
+    console.log(this.state);
 
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+      <input onChange={this.handleChange} type="text"/>
+      <button onClick={() => this.fetchBooks(this.state.search)}>Search</button>
+      {this.state.books.map(book => {
+        return(
+          <div>
+            <h3>{book.title}</h3>
+            <p>{book.description}</p>
+            <p>{book.status}</p>
+          </div>
+        )
+      })
+      }
+        {/* <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
         {this.state.books.length ? (
           <p>Book Carousel coming soon</p>
         ) : (
           <h3>No Books Found :(</h3>
-        )}
+        )} */}
       </>
     )
   }

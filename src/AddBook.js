280ce8c31a65f43e5cec40ee28c7bd2f537bb3ff
Ash Uppal <ashuppal@gmail.com>
2022-12-13 17:38:41 -0800

    added modal

---
+++ src/AddBook.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
+
+export class AddBook extends Component {
+  render() {
+    return (
+      <div>
+        <Modal show={this.props.show} onHide={this.props.hideModal}>
+          <Modal.Body>
+            <Form onSubmit={this.props.handelAddModal}>
+              <Form.Group className='mb-3'>
+                <Form.Label>Book Title</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='title'
+                  placeholder='Enter the title of the book'
+                />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Book Description</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='description'
+                  placeholder='Enter the Description of the Book'
+                />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Book Status</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='status'
+                  placeholder='Enter the status'
+                />
+              </Form.Group>
+            
+              <Button
+                onClick={this.props.hideModal}
+              >
+                Close
+              </Button>
+             
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default AddBook;
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Carousel from 'react-bootstrap/Carousel';
+import AddBook from './AddBook'
 
 class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       books: [],
-      search: ""
+      search: '',
+      showAddModal: false
     }
   }
+  handleDisplayModal = () => {
+    this.setState({ showAddModal: true })
+  }
+
+  hideModal = () => {
+    this.setState({ showAddModal: false })
+  }
+
+  // handelAddModal = e => {
+  //   e.preventDefault()
+  //   const addResult = {
+  //     title: e.target.title.value,
+  //     description: e.target.description.value,
+  //     status: e.target.status.value
+  //   }
+  // }
 
   componentDidMount = () => {
-    this.fetchBooks();
+    this.fetchBooks()
   }
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({ search: event.target.value })
   }
-  fetchBooks = async (bookTitle) => {
-    console.log('fetching book');
-    console.log(bookTitle);
+  fetchBooks = async bookTitle => {
+    console.log('fetching book')
+    console.log(bookTitle)
     let request = {
       method: 'GET',
       url: `${process.env.REACT_APP_SERVER_URL}/books`
@@ -28,45 +46,65 @@ class BestBooks extends React.Component {
     if (bookTitle) {
       request.url += `?title=${bookTitle}`
     }
-    let response = await axios(request);
-    console.log(response);
+    let response = await axios(request)
+    console.log(response)
     this.setState({
-      books: response.data,
-    });
+      books: response.data
+    })
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  handleAddBook = () => {}
 
-  
-  render() {
-    console.log(this.state);
+  render () {
+    console.log(this.state)
 
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
-      <input onChange={this.handleChange} type="text"/>
-      <button onClick={() => this.fetchBooks(this.state.search)}>Search</button>
-      {this.state.books.map(book => {
-        return(
-          <div>
-            <h3>{book.title}</h3>
-            <p>{book.description}</p>
-            <p>{book.status}</p>
-          </div>
-        )
-      })
-      }
-        {/* <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )} */}
+      <input onChange={this.handleChange} type='text' />
+        <button onClick={() => this.fetchBooks(this.state.search)}>
+          Search
+        </button>
+        {this.state.showAddModal && (
+          <>
+            <AddBook
+              show={this.state.showAddModal}
+              hideModal={this.hideModal}
+              handelAddModal={this.handelAddModal}
+            />
+          </>
+        )}
+        <button onClick={this.handleDisplayModal}>Add a book</button>
+
+        
+        {this.state.books.length
+          ? this.state.books.map(book => {
+              return (
+                
+                <div style={{ height: '50%', width: '100%' }}>
+                  <Carousel>
+                    <img
+                      className='d-block w-100'
+                      src='https://via.placeholder.com/800x400'
+                      alt={book.title}
+                    />
+                    <Carousel.Item>
+                      <h3>{book.title}</h3>
+                      <p>{book.description}</p>
+                      <p>{book.status}</p>
+                    </Carousel.Item>
+                  </Carousel>
+                 
+                </div>
+              )
+            })
+          : null}
       </>
     )
   }
 }
 
-export default BestBooks;
+export default BestBooks
